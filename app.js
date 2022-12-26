bfa995ecc1e6fc4164dd87507f022db410d882d5
Anton Malyshev <90730699+TohaLike@users.noreply.github.com>
2022-12-27 01:04:56 +0300

    i add new function

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ const textSection = document.getElementById('text__english');
 const noteBlock = document.querySelector('.note__block');
 const numberAmount = document.querySelector('.number__amount');
 
+let saveText = '';
+
 saveButton.addEventListener('click', () => {
     createNote();
+    console.log(saveText)
 });
 
 function createNote() {
-    
     let note = document.createElement('div');
-    
-    note.innerHTML = textSection.value;    
+    saveText = textSection.value;
+    note.innerHTML = saveText;    
     note.className = 'note__list__main';
+    // note.style.maxWidth = '99%'
     noteBlock.appendChild(note);
     textSection.value = '';
 };
